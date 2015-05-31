cc4d02923e68a6b0e8d2ede858a16d50b8a276b3
Noah Sandman <nsandman09@gmail.com>
2015-05-31 01:24:22 -0700

    Added "n" variants

--- ColorPrinter.h
+++ ColorPrinter.h
@@ -10,6 +10,7 @@
 #define _COLORPRINTER_H
 
 #include <stdarg.h>
+#include <string.h>
 #include <stdio.h>
 
 #define red    31
@@ -20,6 +21,11 @@
 #define cyan   36
 #define white  37
 
+int cprintf(unsigned char, const char*, ...);
+int cfprintf(void*, unsigned char, const char*, ...);
+void cnprintf(unsigned int, unsigned char, const char*, ...);
+void cnfputs(const char*, unsigned char, unsigned int, void*);
+
 // Everything with a fixed number of args is a macro, cuz space = saved. Yay!
 // a = message, b = color
 #define cputs(a, b)     printf("\033[0;%dm%s\033[0m\n", b, a)
@@ -29,6 +35,8 @@
 #define cfputs(a, b, c) fprintf(c, "\033[0;%dm%s\033[0m", b, a)
 #define cfputc(a, b, c) fprintf(c, "\033[0;%dm%c\033[0m", b, a)
 #define cputc(a, b, c)  cfputc(a, b, c) // Same as fcputc() 
+
+#define cnputs(a, b, c) cnfputs(a, b, c, stdout); putchar('\n')
 
 int cprintf(unsigned char color, const char *fmt, ...) {
 	printf("\033[0;%dm", color);
@@ -48,6 +56,25 @@ int cfprintf(void *stream, unsigned char color, const char *fmt, ...) {
 	va_end(args);
 	fputs("\033[0m", stream);
 	return charsWritten;
+}
+
+void cnprintf(unsigned int len, unsigned char color, const char *fmt, ...) {
+	printf("\033[0;%dm", color);
+	char buf[len];
+	va_list args;
+  	va_start(args, fmt);
+  	vsnprintf(buf, len, fmt, args);
+  	fputs(buf, stdout);
+  	va_end(args);
+  	fputs("\033[0m", stdout);
+}
+
+void cnfputs(const char *amsg, unsigned char color, unsigned int len, void *stream) {
+	int y = strlen(amsg);
+	fprintf(stream, "\033[0;%dm", color);
+	for (; len > 0 || len > y; *amsg++, --len)
+		putc(*amsg, stream);
+	fputs("\033[0m", stream);
 }
 
 #endif
--- test.c
+++ test.c
@@ -5,9 +5,12 @@ int main() {
 	cfputs("This is output to stderr!\n", red, stderr);
 	cprintf(purple, "%s works with both of these streams!\n", "Formatting");
 	cfprintf(stderr, blue, "Isn't that %s! And yes, numbers work %d :P\n", "something", 2);
-	cputs("It even works for single characters!", green);
+	cputs("It even works for single characters:", green);
 	cputchar('g', red);
 	cfputc('c', yellow, stdout);
 	cputc('c', cyan, stdout);
 	putchar('\n');
+	cnputs("Use cnputs() to prevent buffer overflow!", red, 40);
+	cnfputs("And, of course, cnfputs() for other streams.\n", blue, 45, stderr);
+	cnprintf(44, cyan, "And use cnprintf() with %s strings!\n", "formatted");
 }
