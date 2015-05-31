d585e94f60969a93ad2d7ff98b321bae0ccbaebc
Noah Sandman <nsandman09@gmail.com>
2015-05-31 00:24:49 -0700

    added to test

--- test.c
+++ test.c
@@ -7,7 +7,7 @@ int main() {
 	fcprintf(stderr, blue, "Isn't that %s! And yes, numbers work %d :P\n", "something", 2);
 	cputs("It even works for single characters!", green);
 	cputchar('g', red);
-	cputchar('c', yellow);
+	fcputc('c', yellow, stdout);
 	cputc('c', cyan, stdout);
 	putchar('\n');
 }
