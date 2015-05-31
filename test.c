fbc91e716684f6140b83a813c8c3d4ebe3ecad7f
Noah Sandman <nsandman09@gmail.com>
2015-05-30 19:11:00 -0700

    Added %d demo

--- test.c
+++ test.c
@@ -4,5 +4,5 @@ int main() {
 	colorPrint(green, "This is output to stdout.");
 	fcolorPrint(stderr, red, "This is output to stderr!");
 	colorPrint(purple, "%s works with both of these streams!", "Formatting");
-	fcolorPrint(stderr, blue, "Isn't that %s!", "something");
+	fcolorPrint(stderr, blue, "Isn't that %s! And yes, numbers work %d :P", "something", 2);
 }
